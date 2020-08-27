f9da58d3914d27a7b3ae871b15ad772c1b8f9f53
Koh Hashimoto <koh.hashimoto@gmail.com>
2020-08-27 13:05:23 +0900

    test update

--- src/Wild.jl
+++ src/Wild.jl
@@ -1,5 +1,9 @@
 module Wild
 
-# Write your package code here.
+Base.getproperty(o::Any, atr::Symbol) =
+    (hasfield(typeof(o), atr)
+     ? Base.getfield(o, atr)
+     : Base.eval(Base.Main, atr)(o))
+#     : (f = Base.eval(Base.Main, atr); (isa(f, AbstTagFunc) ? f : Mth(f))(o)))
 
 end
