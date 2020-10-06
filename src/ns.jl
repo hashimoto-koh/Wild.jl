d218db202348cfdd89bacb5c9ec1d13e8777350b
Koh Hashimoto <koh.hashimoto@gmail.com>
2020-10-06 16:42:35 +0900

    fix

--- src/ns.jl
+++ src/ns.jl
@@ -80,8 +80,9 @@ Base.getproperty(ns::AbstNS, atr::Symbol) =
             error("""This NS does not have a property named "$(atr)".""")
 
         x = d[atr].obj;
-        isa(x, Union{NSPrp, NSMth, NSFnc}) && (return x(ns))
-        isa(x, NSReq) && (y = x(ns); d[atr] = typeof(d[atr])(y); return y)
+        isa(x, Union{NSTagFunc{:prp}, NSTagFunc{:mth}, NSTagFunc{fnc}}) &&
+            (return x(ns))
+        isa(x, NSTagFunc{:req}) && (y = x(ns); d[atr] = typeof(d[atr])(y); return y)
         return x
     end
 
