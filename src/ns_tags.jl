02c64322d1f09ed7128bb7e9c2a49b0391061999
Koh Hashimoto <koh.hashimoto@gmail.com>
2020-10-06 23:25:20 +0900

    fix

--- src/ns_tags.jl
+++ src/ns_tags.jl
@@ -150,7 +150,7 @@ Base.setproperty!(x::NSTag, atr::Symbol, f) =
         ns = x.___NS_ns
         o = ns.__dict[atr].obj
         println(typeof(x), " ", typeof(o))
-        if isa(o, NSTag) && typeof(x).parameters[1] == o.parameters[1]
+        if isa(o, NSTagFunc) && typeof(x).parameters[1] == o.parameters[1]
             __NS_func__add(ns.__dict[atr].obj.fnc, f)
         else
             ns.del(atr)
