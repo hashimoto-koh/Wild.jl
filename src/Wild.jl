58f0ad5deaf86d606da65fbce8a67c715f60abf1
Koh Hashimoto <koh.hashimoto@gmail.com>
2020-10-10 14:56:20 +0900

    @prpをもう一回トライ

--- src/Wild.jl
+++ src/Wild.jl
@@ -3,11 +3,6 @@ module Wild
 include("functionalize.jl")
 export arg, grb, asn, mth, grbs, asns, cry, wc, wd, @cry
 
-# include("core.jl")
-# export AbstTagFunc, AbstPrpFunc, AbstMthFunc
-# export @prp, @mth
-# export @prpfnc, @mthfnc
-
 include("ns_dict0.jl")
 include("ns.jl")
 include("ns_tags.jl")
@@ -26,7 +21,11 @@ include("nscode.jl")
 export AbstNSCode, NSCode
 
 include("operators.jl")
-#=
+
+###############################
+# @prp
+###############################
+
 struct __IsaPrp end
 struct __IsnotaPrp end
 @inline __isprp(x) = __IsnotaPrp()
@@ -35,31 +34,33 @@ struct __IsnotaPrp end
 @inline __asprp(::__IsnotaPrp, f) = o -> ((a...;ka...) -> f(o, a...; ka...))
 
 export @prp
-#=
+
 macro prp(ex)
+    # f
+    if isa(ex, Symbol)
+        f = ex
+        esc(:(@inline Wild.__isprp(::typeof($f)) = Wild.__IsaPrp()))
     # function f(x) 2x end
-    if ex.head == :function
-        eval(esc(ex))
+    elseif ex.head == :function
         f = ex.args[1].args[1]
-    elseif ex.head == :=
-        eval(esc(ex))
+        ff = :(@inline Wild.__isprp(::typeof($f)) = Wild.__IsaPrp())
+        esc(Expr(:toplevel, ex, ff))
+    elseif ex.head == :(=)
         # f(x) = 2x
         if isa(ex.args[1], Expr) && ex.args[1].head == :call
             f = ex.args[1].args[1]
+            ff = :(@inline Wild.__isprp(::typeof($f)) = Wild.__IsaPrp())
+            esc(Expr(:toplevel, ex, ff))
         # f = x -> 2x
         else
             f = ex.args[1]
+            ff = :(@inline Wild.__isprp(::typeof($f)) = Wild.__IsaPrp())
+            esc(Expr(:toplevel, ex, ff))
         end
-    # f
     else
         f = ex
+        esc(:(@inline Wild.__isprp(::typeof($f)) = Wild.__IsaPrp()))
     end
-    :(@inline Wild.__isprp(::typeof(esc(f))) = Wild.__IsaPrp())
-end
-=#
-
-macro prp(f)
-    :(@inline Wild.__isprp(::typeof(esc(f))) = Wild.__IsaPrp())
 end
 
 Base.getproperty(o::Any, atr::Symbol) =
@@ -67,13 +68,5 @@ begin
     hasfield(typeof(o), atr) && (return Base.getfield(o, atr))
     __asprp(Base.eval(Base.Main, atr))(o)
 end
-#=
-Base.getproperty(o::Any, atr::Symbol) =
-    (hasfield(typeof(o), atr)
-     ? Base.getfield(o, atr)
-     : (f = Base.eval(Base.Main, atr);
-        (isa(f, Union{AbstTagFunc, NSTagFunc}) ? f : NSTagFunc{:mth}(f))(o)))
-=#
-=#
 
 end
