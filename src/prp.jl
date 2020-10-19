84452cd8b186d36d1d5f85716fed08a5709a9ff0
Koh Hashimoto <koh.hashimoto@gmail.com>
2020-10-19 14:50:59 +0900

    base_getprp_dict を prp.jlに移動

--- src/prp.jl
+++ src/prp.jl
@@ -61,3 +61,36 @@ end
 @prp Base.length
 @prp Base.adjoint
 @prp Base.transpose
+
+###############################
+# prpdict
+###############################
+
+const base_getprp_dict = Dict{Type, Dict{Symbol, Function}}()
+
+base_getprp_dict[Any] = Dict{Symbol, Function}()
+base_getprp_dict[AbstractArray] = Dict{Symbol, Function}()
+base_getprp_dict[AbstractString] = Dict{Symbol, Function}()
+
+Base.getproperty(o::Any, atr::Symbol) =
+begin
+    hasfield(typeof(o), atr) && (return Base.getfield(o, atr))
+    haskey(base_getprp_dict[Any], atr) && (return base_getprp_dict[Any][atr](o))
+    __asprp(Base.eval(Base.Main, atr))(o)
+end
+
+Base.getproperty(o::AbstractArray, atr::Symbol) =
+begin
+    hasfield(typeof(o), atr) && (return Base.getfield(o, atr))
+    haskey(base_getprp_dict[AbstractArray], atr) &&
+        (return base_getprp_dict[AbstractArray][atr](o))
+    __asprp(Base.eval(Base.Main, atr))(o)
+end
+
+Base.getproperty(o::AbstractString, atr::Symbol) =
+begin
+    hasfield(typeof(o), atr) && (return Base.getfield(o, atr))
+    haskey(base_getprp_dict[AbstractString], atr) &&
+        (return base_getprp_dict[AbstractString][atr](o))
+    __asprp(Base.eval(Base.Main, atr))(o)
+end
